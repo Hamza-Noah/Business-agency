fa5f6782c0c499477addb1fe5e201c61e26ba16c
Hamza-Noah <hamza.nouh97@gmail.com>
2022-07-27 10:47:51 +0200

    adding the countdown

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,24 +1,46 @@
 $(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
-      loop: false,
-      margin: 10,
-      nav: false,
-      dots: false,
-  
-      responsive: {
-        0: {
-          items: 1,
-          autoplay: true,
-          dots: true,
-        },
-        768: {
-          items: 2,
-          autoplay: true,
-          dots: true,
-        },
-        1000: {
-          items: 5,
-        },
+  $(".owl-carousel").owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: false,
+    dots: false,
+
+    responsive: {
+      0: {
+        items: 1,
+        autoplay: true,
+        dots: true,
       },
-    });
+      768: {
+        items: 2,
+        autoplay: true,
+        dots: true,
+      },
+      1000: {
+        items: 5,
+      },
+    },
   });
+});
+
+// Incrementing
+let nums = document.querySelectorAll(".num");
+let section = document.querySelector(".statistics");
+let started = false; // Function Started ? No
+
+
+window.onscroll = function () {
+  nums.forEach((num) => {
+    startCount(num);
+  });
+};
+
+function startCount(num) {
+  let goal = num.dataset.goal;
+  let count = setInterval(() => {
+    num.textContent++;
+    if (num.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
